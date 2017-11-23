3bc9c8e21a1aa2fc3de674d8a7960f1804d957c1
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-23 18:54:47 +0800

    增加Post::truncate()

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -11,6 +11,9 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
+        Post::truncate();
+
+
         foreach(range(1,20) as $number){
             Post::create([
                 'title' => 'title '.$number,
