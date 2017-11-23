e1189e32b6466639ff23d0e1ce4df0eff4f4db92
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-23 17:56:19 +0800

    使用 find 方法

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,10 @@ Route::get('/test', function () {
        $post -> content ='test content';
        $post -> save();
     */
-
+/*
        $posts = \App\Post::all();
        dd($posts);
-
+*/
+    $post=\App\Post::find(4);
+    dd($post);
 });
