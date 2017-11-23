cf3513fbbbff7b3d3f56e305305b104ed7270f93
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-23 18:47:00 +0800

    錯誤更正:開啟 Comment.php，增加關聯

--- app/Comment.php
+++ app/Comment.php
@@ -3,5 +3,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
-    //
+    protected $table = 'comments';
+    protected $fillable=['title', 'content_2', 'is_feature'];
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
