8b8d3e70d867719d45e6da90aa3e1be62a0b45be
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-23 18:14:16 +0800

    使用 destroy方法刪除id為2的資料

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,9 @@ Route::get('/test', function () {
     $post->content= 'saved content';
     $post->save();
     */
+    /*
     $post= \App\Post::find(1);
     $post->delete();
-});
+    */
+    \App\Post::destroy(2);
+    });
