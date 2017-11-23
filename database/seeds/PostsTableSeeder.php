1e748fa27095a4ce9178c4375138842464ffadf4
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-23 19:01:56 +0800

    使用 Faker 讓資料更擬真

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Post;
 use Carbon\Carbon;
+use Faker\Factory as Faker;
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -18,11 +19,15 @@ class PostsTableSeeder extends Seeder
         foreach(range(1,20) as $number){
             $total=20;
             Post::create([
+                /*
                 'title' => 'title '.$number,
                 'content' => 'content '.$number,
                 'is_feature' => rand(0,1),
                 'created_at' => Carbon::now() ->subDays($total - $number),
                 'updated_at' => Carbon::now() ->subDays($total - $number),
+                */
+                'title' => $faker->sentence,
+                'content' => $faker->paragraph,
             ]);
         }
     }
