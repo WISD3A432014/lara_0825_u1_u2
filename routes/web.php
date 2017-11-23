771eef1469503c64dbf61c7e10dbc0255ef846fc
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-23 18:43:31 +0800

    透過關聯將資料印出來

--- routes/web.php
+++ routes/web.php
@@ -81,8 +81,15 @@ Route::get('/test', function () {
     $lastPost = \App\Post::orderBy('id','DESC') ->first();
     dd($lastPost);
     */
+    $post = \App\Post::find(1);
+    foreach ($post->comments as $comment){
+        echo $comment->content.'<br>';
+    }
+
+    /*
     $comment = new \App\Comment();
     $comment -> title = 'test title';
     $comment -> content ='test content';
     $comment -> save();
+    */
     });
